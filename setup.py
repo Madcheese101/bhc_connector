6b730d92fbc6c2ef31986a0029bc85db5f2d1fb0
Madcheese101 <hax.grenade@gmail.com>
2025-10-27 01:59:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-   from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-   from pip.req import parse_requirements
-import re, ast
 
-# get version from __version__ variable in woocommerceconnector/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('woocommerceconnector/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
+from woocommerceconnector import __version__ as version
 
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
